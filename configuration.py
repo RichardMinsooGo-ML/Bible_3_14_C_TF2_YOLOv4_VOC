74fc9835c6b87083c59e4cc241142cf0bbd4a329
Richard Minsoo Go <67931944+RichardMinsooGo-ML@users.noreply.github.com>
2022-10-15 12:36:27 +0900

    Update configuration.py

--- configuration.py
+++ configuration.py
@@ -7,11 +7,9 @@ YOLO_CUSTOM_WEIGHTS   = False # "checkpoints/yolov3_custom" # used in evaluate_m
                              # YOLO_CUSTOM_WEIGHTS also used with TensorRT and custom model detection    
 
 # YOLO options
-YOLO_TYPE = "yolov3" # yolov4 or yolov3
+YOLO_TYPE = "yolov4" # yolov4 or yolov3
 YOLO_FRAMEWORK = "tf" # "tf" or "trt"
 
-YOLO_V3_WEIGHTS             = "./checkpoints/yolov3.weights"
-YOLO_V3_TINY_WEIGHTS        = "./checkpoints/yolov3-tiny.weights"
 YOLO_V4_WEIGHTS             = "./checkpoints/yolov4.weights"
 YOLO_V4_TINY_WEIGHTS        = "./checkpoints/yolov4-tiny.weights"
 YOLO_COCO_CLASSES           = "./dataset/coco.names"
@@ -19,10 +17,8 @@ YOLO_COCO_CLASSES           = "./dataset/coco.names"
 TRAIN_CLASSES               = "./dataset/voc/voc2012.names"
 TRAIN_ANNOT_PATH            = "./dataset/voc/VOC2012_train.txt"
 TEST_ANNOT_PATH             = "./dataset/voc/VOC2012_val.txt"
-if YOLO_TYPE == "yolov3":
-    TRAIN_CHECKPOINTS_FOLDER    = "./checkpoints"
-    DATA_TYPE = "yolo_v3_voc"
-elif YOLO_TYPE == "yolov4":
+
+YOLO_TYPE == "yolov4":
     TRAIN_CHECKPOINTS_FOLDER    = "./checkpoints"
     DATA_TYPE = "yolo_v4_voc"
     
@@ -41,10 +37,6 @@ if YOLO_TYPE                == "yolov4":
                                [[36,  75], [76,   55], [72,  146]],
                                [[142,110], [192, 243], [459, 401]]]
 
-if YOLO_TYPE                == "yolov3":
-    YOLO_ANCHORS            = [[[10,  13], [16,   30], [33,   23]],
-                               [[30,  61], [62,   45], [59,  119]],
-                               [[116, 90], [156, 198], [373, 326]]]
 # Train options
 TRAIN_SAVE_BEST_ONLY        = True # saves only best model according validation loss (True recommended)
 TRAIN_SAVE_CHECKPOINT       = False # saves all best validated checkpoints in training process (may require a lot disk space) (False recommended)
